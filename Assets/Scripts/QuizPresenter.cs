3b39582ea16190d1044e2ddcd53cd51d4e470189
Rafael Hedman <rafael@rafael.se>
2025-02-21 06:35:54 +0100

    Populate answer buttons, subscribe to click events

--- Assets/Scripts/QuizPresenter.cs
+++ Assets/Scripts/QuizPresenter.cs
@@ -1,25 +1,47 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace BoardGameQuiz
 {
 	public class QuizPresenter : MonoBehaviour
 	{
 		public TMP_Text questionField;
+		public List<Button> answerButtons = new();
 
 		private void Awake()
 		{
-			//GetComponent<QuizPresenter>().enabled = false;
 			GetComponent<Canvas>().enabled = false;
 		}
 
 		public void DisplayQuiz(Quiz quizData)
 		{
-			//enabled = true;
-			//GetComponent<QuizPresenter>().enabled = true;
 			GetComponent<Canvas>().enabled = true;
 
 			questionField.text = quizData.Question;
+
+			Assert.IsTrue(quizData.Answers.Count == answerButtons.Count);
+
+			var answerCount = Math.Min(quizData.Answers.Count, answerButtons.Count);
+
+			for (var index = 0; index < answerCount; index++)
+			{
+				var answer = quizData.Answers[index];
+				var answerButton = answerButtons[index];
+				var answerButtonTextField = answerButton.GetComponentInChildren<TextMeshProUGUI>();
+				answerButtonTextField.text = answer.Text;
+
+				var answerIndex = index;
+				answerButton.onClick.AddListener(() => SelectAnswer(answerIndex));
+			}
+		}
+
+		private void SelectAnswer(int answerIndex)
+		{
+			Debug.Log($"Selected answer index: {answerIndex}");
 		}
 	}
 }
